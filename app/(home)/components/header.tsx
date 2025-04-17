444419173aab036aed18b8e4e0f64f30ab5894d1
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-04-17 12:54:50 +0800

    save

--- app/(home)/components/header.tsx
+++ app/(home)/components/header.tsx
@@ -1,14 +1,14 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 
 export const Header = () => {
+  const { } = useSession()
   return (
-    <div>
-
+    <header className='h-16 border-b border-gray-200 bg-white flex items-center'>
       <Button onClick={() => signOut()}>Sign out</Button>
       Header
-    </div>
+    </header>
   )
 }
