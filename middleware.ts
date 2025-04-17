8d29924359d2f35b2ae90dcb7a6b40e873f91629
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-04-17 12:28:13 +0800

    save

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,23 @@
 import {auth} from "@/auth"
-import { NextRequest, NextResponse } from "next/server";
-import { getSession } from "./actions/getCurrentSession";
+// import { NextRequest, NextResponse } from "next/server";
+// import { getSession } from "./actions/getCurrentSession";
 
-// export default auth((req) => {
-//     const isLoginPage = req.nextUrl.pathname.startsWith('/login')
+export default auth((req) => {
+    const isAuthUser = req.auth;
 
-//     const isAuthUser = req.auth;
+        if(!isAuthUser) {
+            return Response.redirect(new URL('/login', req.url))
+        }
+}) 
 
-//         if(!isAuthUser) {
-//             return Response.redirect(new URL('/login', req.url))
-//         }
-// }) 
-
-export async function middleware(request: NextRequest) {
-    const isLoginPage = request.nextUrl.pathname.startsWith('/login')
-    const session = await getSession()
-    if(!session) {
-    return Response.redirect(new URL('/login', request.url))
-    }
+// export async function middleware(request: NextRequest) {
+//     const isLoginPage = request.nextUrl.pathname.startsWith('/login')
+//     const session = await getSession()
+//     if(!session) {
+//     return Response.redirect(new URL('/login', request.url))
+//     }
     
-  }
-
+//   }
  
 export const config = {
     matcher: [
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // async redirects() {
+  //       return [
+  //         {
+  //           source: '/api/v1/media/:path*',
+  //           destination: 'https://github.com',
+  //           permanent: true
+  //         }
+  //       ]
+  // },
 };
 
 export default nextConfig;
